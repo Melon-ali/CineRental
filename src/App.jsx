dd889ac9bf629a4e1c71530f021ba26b85d4eafa
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2024-08-22 19:24:55 +0600

    cine rentel movie site added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
