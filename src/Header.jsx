0fe9cd4aa9e1be8fab39503d0b555ccf6b1cd352
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2024-08-24 11:55:14 +0600

    cart details section added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Moon from "./assets/icons/moon.svg";
 import Logo from "./assets/logo.svg";
 import Ring from "./assets/ring.svg";
 import ShoppingCart from "./assets/shopping-cart.svg";
+import CartDetails from "./cine/CartDetails";
 
 const Header = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  const handleCartShow = () => {
+    setShowCart(true);
+  };
+
   return (
     <header>
+      {showCart && <CartDetails onClose={() => setShowCart(false)} />}
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={Logo} width="139" height="26" alt="logo" />
@@ -25,25 +34,16 @@ const Header = () => {
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
-              <img
-                src={Moon}
-                width="24"
-                height="24"
-                alt="moon"
-              />
+              <img src={Moon} width="24" height="24" alt="moon" />
             </a>
           </li>
           <li>
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={handleCartShow}
             >
-              <img
-                src={ShoppingCart}
-                width="24"
-                height="24"
-                alt="cart"
-              />
+              <img src={ShoppingCart} width="24" height="24" alt="cart" />
             </a>
           </li>
         </ul>
